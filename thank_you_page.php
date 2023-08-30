214fd36961ab9f00e14c15c6772b67d90818de4f
Joeyyy09 <imt_2021063@iiitm.ac.in>
2023-08-30 14:44:59 +0530

    Fix Responsiveness

---
+++ thank_you_page.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thank You</title>
+</head>
+<body>
+    <h1>Thank You for Submitting Your Message</h1>
+    <p>Your message has been received and will be processed.</p>
+</body>
+</html>
