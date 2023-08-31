62d25b83d4a4a9dad38a9fa01eabdfe3d84f78f9
Joeyyy09 <imt_2021063@iiitm.ac.in>
2023-08-31 11:11:55 +0530

    Update PHP code

--- thank_you_page.php
+++ thank_you_page.php
@@ -7,20 +7,25 @@ $thankYouMessage = "";
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Retrieve form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-    
-    // Validate and process the data (you can add more validation if needed)
+    // Retrieve and sanitize form data
+    $name = sanitizeInput($_POST["name"]);
+    $email = sanitizeInput($_POST["email"]);
+    $message = sanitizeInput($_POST["message"]);
+
+    // Validate and process the data
     if (!empty($name) && !empty($email) && !empty($message)) {
         // Process the data (e.g., send an email, store in a database, etc.)
-        
+
         // Display thank you message
         $thankYouMessage = "Thank you for your submission!";
     } else {
         $thankYouMessage = "Please fill in all the fields.";
     }
+}
+
+// Function to sanitize input data
+function sanitizeInput($input) {
+    return trim(htmlspecialchars($input, ENT_QUOTES, "UTF-8"));
 }
 ?>
 
@@ -35,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <?php if (!empty($thankYouMessage)) : ?>
         <p><?php echo $thankYouMessage; ?></p>
     <?php else : ?>
-        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8"); ?>">
             <label for="name">Name:</label><br>
             <input type="text" id="name" name="name" value="<?php echo $name; ?>"><br>
             
